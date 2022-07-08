070631779776095df878a58c3d73a8cac45f46dc
Prabin Acharya <acharyaprabin101@gmail.com>
2022-07-08 07:12:21 +0545

    apply grayscale filter

--- script.js
+++ script.js
@@ -8,5 +8,26 @@ const image = new Image();
 image.src = "DALLE.png";
 
 image.addEventListener("load", () => {
-  ctx.drawImage(image, 0, 0, 512, 512);
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+  const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  const scannedImage_grayscale = grayscale(scannedImage);
+
+  ctx.putImageData(scannedImage_grayscale, 0, 0);
 });
+
+//apply grayscale filter
+function grayscale(scannedImage) {
+  const scannedData = scannedImage.data;
+
+  for (let i = 0; i < scannedData.length; i += 4) {
+    const total = scannedData[i] + scannedData[i + 1] + scannedData[i + 2];
+    const averageColorValue = total / 3;
+    scannedData[i] = averageColorValue;
+    scannedData[i + 1] = averageColorValue;
+    scannedData[i + 2] = averageColorValue;
+  }
+  scannedImage.data = scannedData;
+  return scannedImage;
+}
