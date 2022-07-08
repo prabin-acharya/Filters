d78b9e55ac93a94c07f58e273ac2b56c1253f411
Prabin Acharya <acharyaprabin101@gmail.com>
2022-07-08 19:11:50 +0545

    whoww that was a lot. Added reflect image

--- script.js
+++ script.js
@@ -21,10 +21,21 @@ image.addEventListener("load", () => {
 
   // toGrayscale(scannedImage);
 
-  toSepia(scannedImage);
+  // toSepia(scannedImage);
+
+  // toNegative(scannedImage);
+
+  reflectImage(scannedImage);
 
   displayImage(scannedImage);
 });
+
+//display image
+function displayImage(image) {
+  ctx.putImageData(image, 0, 0);
+}
+
+//Filters
 
 //apply grayscale filter
 function toGrayscale(scannedImage) {
@@ -62,7 +73,51 @@ function toSepia(scannedImage) {
   }
 }
 
-//display image
-function displayImage(image) {
-  ctx.putImageData(image, 0, 0);
+//apply Negative filter
+function toNegative(scannedImage) {
+  const scannedData = scannedImage.data;
+
+  for (let i = 0; i < scannedData.length; i += 4) {
+    const red = scannedData[i];
+    const green = scannedData[i + 1];
+    const blue = scannedData[i + 2];
+
+    scannedData[i] = 255 - red;
+    scannedData[i + 1] = 255 - green;
+    scannedData[i + 2] = 255 - blue;
+  }
+}
+
+//filter to reflect the image
+function reflectImage(scannedImage) {
+  const scannedData = scannedImage.data;
+
+  for (let i = 0; i < scannedData.length / (canvas.width * 4); i++) {
+    for (let j = 0; j < (canvas.width * 4) / 2; j += 4) {
+      swapPixels(
+        scannedData,
+        canvas.width * i * 4 + j,
+        canvas.width * i * 4 + canvas.width * 4 - 4 - j
+      );
+    }
+  }
+}
+
+function swapPixels(scannedData, pixel1, pixel2) {
+  const tempPixel1 = {
+    red: scannedData[pixel1],
+    green: scannedData[pixel1 + 1],
+    blue: scannedData[pixel1 + 2],
+    alpha: scannedData[pixel1 + 3],
+  };
+
+  scannedData[pixel1] = scannedData[pixel2];
+  scannedData[pixel1 + 1] = scannedData[pixel2 + 1];
+  scannedData[pixel1 + 2] = scannedData[pixel2 + 2];
+  scannedData[pixel1 + 3] = scannedData[pixel2 + 3];
+
+  scannedData[pixel2] = tempPixel1.red;
+  scannedData[pixel2 + 1] = tempPixel1.green;
+  scannedData[pixel2 + 2] = tempPixel1.blue;
+  scannedData[pixel2 + 3] = tempPixel1.alpha;
 }
